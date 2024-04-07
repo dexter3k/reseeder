21ebcbb85e89af8a55dc7f54e8e0f2515ba26761
dexter3k <schooldev3000@gmail.com>
2024-04-07 23:26:42 +0300

    Added basic tailwind stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello, GitHub Pages!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <div class="flex">
+    	<div class="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900">
+		<FaPlus class="relative flex items-center justify-center
+			       h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
+			       hover:rounded-xl
+			       bg-gray-800 text-green-500
+			       hover:bg-green-600 hover:text-white"/>
+	</div>
+    </div>
+  );
 }
 
 export default App
