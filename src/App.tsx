49f7ac4df7c2e02ca942a1f6813be4d704fe1573
dexter3k <schooldev3000@gmail.com>
2024-04-07 23:28:44 +0300

    Fixed build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 function App() {
   return (
-    <div class="flex">
-    	<div class="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900">
-		<FaPlus class="relative flex items-center justify-center
+    <div className="flex">
+    	<div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900">
+		<FaPlus className="relative flex items-center justify-center
 			       h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
 			       hover:rounded-xl
 			       bg-gray-800 text-green-500
