2d2d5a1e0e499015027a428a3c389080aa36545e
dexter3k <schooldev3000@gmail.com>
2024-04-12 02:20:09 +0300

    Playing around with some css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { FaPlus } from 'react-icons/fa';
+import { TbBox } from 'react-icons/tb';
+
+function BarButton({children}) {
+  return (
+    <div className="flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto
+                    bg-gray-100 hover:bg-red-600 text-red-500 hover:text-white
+                    rounded-3xl hover:rounded-xl
+                    transition-all duration-0 hover:duration-[50ms] ease-linear cursor-pointer shadow-xl">
+      {children}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="flex">
-    	<div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900">
-		<FaPlus className="relative flex items-center justify-center
-			       h-12 w-12 mt-2 mb-2 mx-auto shadow-lg
-			       hover:rounded-xl
-			       bg-gray-800 text-green-500
-			       hover:bg-green-600 hover:text-white"/>
-	</div>
+      <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-green-600">
+        <BarButton><TbBox className="size-10"/></BarButton>
+        <hr className="bg-gray-200 dark:bg-red-600
+                       mx-2"/>
+        <BarButton><FaPlus className="size-6" /></BarButton>
+        <BarButton><FaPlus className="size-6" /></BarButton>
+        <BarButton><FaPlus className="size-6" /></BarButton>
+        <BarButton><FaPlus className="size-6" /></BarButton>
+        <BarButton><FaPlus className="size-6" /></BarButton>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
 import App from './App.tsx'
+
 import './index.css'
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+  },
+];
+
+const router = createBrowserRouter(routes, {
+  basename: '/reseeder/'
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
